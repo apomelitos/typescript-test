54003171729f0c95b24388349a5c170cfe4698f6
Roman Dyadyushkin <rd.rd.2250@gmail.com>
2022-01-31 13:39:39 +0200

    initial commit

---
+++ my-app/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { YesIAmApp as NeApp } from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <NeApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
