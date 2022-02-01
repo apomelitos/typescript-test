cf2210452e27f05f85fa05471a91d7c0eaeb23cc
Roman Dyadyushkin <rd.rd.2250@gmail.com>
2022-02-01 17:47:15 +0200

    Add PokemonDetails component

---
+++ my-app/src/components/PokemonDetails.tsx
@@ -0,0 +1,38 @@
+import { FC, useState, useEffect } from 'react';
+import { PokemonType } from '../types';
+import { EvolutionList } from './EvolutionsList';
+import './PokemonDetails.scss';
+
+type PokemonDetailsProps = {
+  id: number;
+  baseURL: string;
+};
+
+export const PokemonDetails: FC<PokemonDetailsProps> = ({ id, baseURL }): JSX.Element => {
+  const [pokemon, setPokemon] = useState<PokemonType>();
+
+  useEffect(() => {
+    const fetchPokemonById = async (id: number) => {
+      const response = await fetch(`${baseURL}/pokemon/${id}`);
+      const data = (await response.json()) as PokemonType;
+      setPokemon(data);
+    };
+
+    fetchPokemonById(id);
+  }, [id, baseURL]);
+
+  return (
+    <div className='pokemon-details'>
+      <h2>{pokemon?.name}</h2>
+      <div className='img-wrapper'>
+        <img src={pokemon?.sprites.front_default} alt={pokemon?.name} />
+      </div>
+      <div>
+        <p>Weight: {pokemon?.weight}</p>
+        <p>Height: {pokemon?.height}</p>
+        <p>Base experience: {pokemon?.base_experience}</p>
+      </div>
+      {pokemon && <EvolutionList pokemonSpeciesURL={pokemon?.species?.url} />}
+    </div>
+  );
+};
