d33b3e54153a15d02868d9507b4743ec1ad86133
Roman Dyadyushkin <rd.rd.2250@gmail.com>
2022-02-02 18:15:42 +0200

    Try to use type guard

--- src/components/EvolutionsList.tsx
+++ src/components/EvolutionsList.tsx
@@ -1,12 +1,36 @@
 import { FC, useCallback, useEffect, useState } from 'react';
+import { getIdFromURL } from '../utils/helpers';
 import { PokemonType, EvolutionChain, EvolutionsListProps } from '../types';
 import './EvolutionsList.scss';
+
+type SpecieType = {
+  evolution_chain: { url: string };
+};
+
+type EvolutionChainResponse = {
+  chain: EvolutionChain;
+};
+
+const fetchPokemonSpritesByName = async (URLs: string[]): Promise<string[]> => {
+  try {
+    const promises = URLs.map((url) => fetch(url));
+    const responses = await Promise.all(promises);
+    const jsonPromises = responses.map((resp) => resp.json());
+    const pokemons = (await Promise.all(jsonPromises)) as PokemonType[];
+
+    return pokemons.map((pokemon) => pokemon.sprites.front_default);
+  } catch (err) {
+    console.error(err);
+  }
+
+  return [];
+};
 
 export const EvolutionList: FC<EvolutionsListProps> = ({ pokemonSpeciesURL }) => {
   const [evolutions, setEvolutions] = useState<EvolutionChain[]>();
 
   const getArrayFromRecursiveObject = (obj: EvolutionChain): EvolutionChain[] => {
-    const arr: Array<EvolutionChain> = [];
+    const arr: EvolutionChain[] = [];
 
     let currentEvolution: EvolutionChain = obj;
 
@@ -26,37 +50,24 @@ export const EvolutionList: FC<EvolutionsListProps> = ({ pokemonSpeciesURL }) =>
     return arr;
   };
 
-  const fetchPokemonSpritesByName = async (URLs: string[]): Promise<string[]> => {
-    try {
-      const promises = URLs.map((url) => fetch(url));
-      const responses = await Promise.all(promises);
-      const jsonPromises = responses.map((resp) => resp.json());
-      const pokemons = (await Promise.all(jsonPromises)) as PokemonType[];
-      return pokemons.map((pokemon) => pokemon.sprites.front_default);
-    } catch (err) {
-      console.error(err);
-    }
-
-    return [];
-  };
-
   const fetchEvolutions = useCallback(async () => {
     const speciesResponse = await fetch(pokemonSpeciesURL);
-    const {
-      evolution_chain: { url },
-    } = (await speciesResponse.json()) as { evolution_chain: { url: string } };
 
-    const evolutionResponse = await fetch(url);
-    const { chain } = (await evolutionResponse.json()) as { chain: EvolutionChain };
+    if (speciesResponse.ok) {
+      const { evolution_chain } = (await speciesResponse.json()) as SpecieType;
+      const evolutionResponse = await fetch(evolution_chain.url);
 
-    const evolutionsList = getArrayFromRecursiveObject(chain);
+      if (evolutionResponse.ok) {
+        const { chain } = (await evolutionResponse.json()) as EvolutionChainResponse;
+        const evolutionsList = getArrayFromRecursiveObject(chain);
+        const urls: string[] = evolutionsList.map((item) => `https://pokeapi.co/api/v2/pokemon/${item.species.name}`);
+        const sprites: string[] = await fetchPokemonSpritesByName(urls);
 
-    const urls: string[] = evolutionsList.map((item) => `https://pokeapi.co/api/v2/pokemon/${item.species.name}`);
-    const sprites: string[] = await fetchPokemonSpritesByName(urls);
+        evolutionsList.forEach((item, index, arr) => (arr[index].sprite = sprites[index]));
 
-    evolutionsList.forEach((item, index, arr) => (arr[index].sprite = sprites[index]));
-
-    setEvolutions(evolutionsList);
+        setEvolutions(evolutionsList);
+      }
+    }
   }, [pokemonSpeciesURL]);
 
   useEffect(() => {
@@ -66,8 +77,8 @@ export const EvolutionList: FC<EvolutionsListProps> = ({ pokemonSpeciesURL }) =>
   return (
     <ul className='evolution-chain'>
       {evolutions &&
-        evolutions.map((item, idx) => (
-          <li key={idx}>
+        evolutions.map((item) => (
+          <li key={item.sprite && getIdFromURL(item.sprite)}>
             <h4>
               {item.species.name} {item.is_baby ? '(baby)' : ''}
             </h4>
